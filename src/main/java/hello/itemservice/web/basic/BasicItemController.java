4337678b0290b5f4fdfcc2ad20202686b8fee286
eunji <qkrdmswl1018@naver.com>
2023-04-05 16:20:01 +0900

    상품 등록

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,10 +7,7 @@ import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -24,18 +21,56 @@ public class BasicItemController {
     // BasicItemController가 스프링빈에 등록되면서 생성자주입 itemRepository
 
     @GetMapping
-    public String items(Model model){
+    public String items(Model model) {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "basic/items";
     }
 
     @GetMapping("/{itemId}")
-    public String item(@PathVariable long itemId, Model model){
+    public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
         return "basic/item";
     }
+
+    @GetMapping("/add") // 조회
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    // 위아래 같은 url인데 HTTPMethod로 url 구분!
+
+//    @PostMapping("/add")
+//    public String addItemV1(@RequestParam String itemName,
+//                            @RequestParam int price,
+//                            @RequestParam Integer quantity,
+//                            Model model) {
+//        Item item = new Item();
+//        item.setItemName(itemName);
+//        item.setPrice(price);
+//        item.setQuantity(quantity);
+//
+//        itemRepository.save(item);
+//        model.addAttribute("item", item);
+//
+//        return "basic/item";
+//    }
+//
+////    @PostMapping("/add") 위랑 중복 매핑 일어나서 오류날까 봐 주석처리!
+//    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+//        itemRepository.save(item);
+////        model.addAttribute("item", item); // 자동 추가, 생략 가능
+//        return "basic/item";
+//    }
+
+    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        // @ModelAttribute 생략 가능
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
     /**
      * 테스트용 데이터 추가
      */
