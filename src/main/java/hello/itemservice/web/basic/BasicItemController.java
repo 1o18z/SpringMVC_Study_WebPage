666dd138e837d7988cad9421087fd62af0cd2d34
eunji <qkrdmswl1018@naver.com>
2023-04-05 18:58:00 +0900

    PRG(Post/Redirect/Get)와 RedirectAttributes

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -42,33 +43,71 @@ public class BasicItemController {
     // 위아래 같은 url인데 HTTPMethod로 url 구분!
 
 //    @PostMapping("/add")
-//    public String addItemV1(@RequestParam String itemName,
-//                            @RequestParam int price,
-//                            @RequestParam Integer quantity,
-//                            Model model) {
-//        Item item = new Item();
-//        item.setItemName(itemName);
-//        item.setPrice(price);
-//        item.setQuantity(quantity);
-//
-//        itemRepository.save(item);
-//        model.addAttribute("item", item);
-//
-//        return "basic/item";
-//    }
-//
-////    @PostMapping("/add") 위랑 중복 매핑 일어나서 오류날까 봐 주석처리!
-//    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
-//        itemRepository.save(item);
-////        model.addAttribute("item", item); // 자동 추가, 생략 가능
-//        return "basic/item";
-//    }
+    public String addItemV1(@RequestParam String itemName,
+                            @RequestParam int price,
+                            @RequestParam Integer quantity,
+                            Model model) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
 
-    @PostMapping("/add")
+        itemRepository.save(item);
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+////    @PostMapping("/add") 위랑 중복 매핑 일어나서 오류날까 봐 주석처리!
+    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+        itemRepository.save(item);
+        model.addAttribute("item", item); // 자동 추가, 생략 가능
+        return "basic/item";
+    }
+
+    //    @PostMapping("/add")
     public String addItemV3(@ModelAttribute Item item) {
         // @ModelAttribute 생략 가능
         itemRepository.save(item);
         return "basic/item";
+    }
+
+    //    @PostMapping("/add")
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    //    @PostMapping("/add")
+    public String addItemV5(Item item) {
+        itemRepository.save(item);
+        return "redirect:/basic/items" + item.getId();
+        // 수정 후 새로고침하면 계속 Post 실행됨 (마지막 수행된 게 계속 다시 실행돼서) -> 상품 상세 페이지로 리다이렉트 보내서 이걸 방지
+        // 새로고침해서 다시 요청이 오면 "redirect:/basic/items" + item.getId() 여기로 가라고 다시 요청을 해버림!
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) { // 저장되었다는 메시지를 같이 보내자 (파라미터를 붙여서 보냄)
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}"; // 여기 못 들어간 애(status)는 쿼리 파라미터 형식(?status=true)로 들어감
+    } // addAttribute 덕분에 itemId를 치환을 해준다({itemId} 이렇게!)
+
+    // 상품 수정 폼 뷰
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId); // 아이디로 찾고
+        model.addAttribute("item", item); // 모델에 넣고
+        return "basic/editForm";
+    }
+
+    // 상품 수정 처리
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item); // 수정 처리
+        return "redirect:/basic/items/{itemId}";
+        // 마지막에 뷰 템플릿을 호출하는 대신 상품 상세 화면으로 이동하도록 리다이렉트 호출
     }
 
     /**
